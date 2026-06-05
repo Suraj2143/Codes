ef37d7429411e72037d1c55521bba7209961cff5
Suraj yadav <86047026+Suraj2143@users.noreply.github.com>
2026-06-06 02:15:06 +0530

    Time: 12 ms (21.76%) | Memory: 52.3 MB (20.52%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+
+                if (c == '.') continue;
+
+                String row = c + " in row " + i;
+                String col = c + " in col " + j;
+                String box = c + " in box " + (i / 3) + "-" + (j / 3);
+
+                if (!seen.add(row) || !seen.add(col) || !seen.add(box)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
