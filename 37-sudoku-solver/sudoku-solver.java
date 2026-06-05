f6c91c58c6b51b24eb68f57be80662759bdd353a
Suraj yadav <86047026+Suraj2143@users.noreply.github.com>
2026-06-06 02:16:16 +0530

    Time: 210 ms (68.41%) | Memory: 43 MB (16.50%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,46 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                if (board[i][j] == '.') {
+
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+
+                            if (solve(board)) return true;
+
+                            board[i][j] = '.';
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c) return false;
+            if (board[i][col] == c) return false;
+        }
+
+        int r = (row / 3) * 3;
+        int c0 = (col / 3) * 3;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[r + i][c0 + j] == c) return false;
+            }
+        }
+
+        return true;
+    }
+}
